41afabed8d85bb760121b2e4ec30bc73e2fce4d4
Shinyoung Kim <rolroralra@gmail.com>
2021-04-02 17:26:23 +0900

    Update closure_test.go

--- function/closure/closure_test.go
+++ function/closure/closure_test.go
@@ -18,3 +18,18 @@ func TestFibonacci(t *testing.T) {
 		fmt.Printf("Fibonacci(%d) = %v\n", i, f())
 	}
 }
+
+func calc(a int, b int) func(int) int {
+	return func(x int) int {
+		return a * x + b
+	}
+}
+
+func TestClosure(t *testing.T) {
+	a, b := 1, 2
+	functionVariable := calc(a, b)
+	fmt.Println(functionVariable(1))
+	a, b = 2, 3
+	functionVariable = calc(a, b)
+	fmt.Println(functionVariable(1))
+}
